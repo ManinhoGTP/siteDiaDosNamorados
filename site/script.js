920313cd3db5fdc9ad5bcbfbafa4fd5c6c3f04ba
ManinhoGTP <gabrieltuler99@gmail.com>
2026-05-28 19:51:36 -0300

    +

--- site/script.js
+++ site/script.js
@@ -1,3 +1,5 @@
+const relogio = document.getElementById("relogio")
+
 function mostrarSection(sectionId){
 
 
@@ -10,3 +12,19 @@ function mostrarSection(sectionId){
     const sectionAtiva = document.getElementById(sectionId)
     sectionAtiva.classList.add("ativa")
 }
+
+
+const tempo = setInterval(function() {
+    const agora = new Date()
+    const inicioDoNm = new Date("Apr 03 2026 17:41:00")
+    const diferença = agora - inicioDoNm
+    
+
+
+    const d = Math.floor(diferença / (1000 * 60 * 60 * 24))
+    const h = Math.floor((diferença % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    const m = Math.floor((diferença % (1000 * 60 * 60)) / (1000 * 60))
+    const s = Math.floor((diferença % (1000 * 60)) / 1000)
+
+    relogio.innerHTML = `${d}d ${h}h ${m}m ${s}s`
+}, 1000)
